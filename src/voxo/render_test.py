ec61106ccc396127116d62473249ec477e6163b3
David Westreicher <david.westreicher@gmail.com>
2026-07-08 00:06:33 +0200

    Raytrace Voxel POC

--- src/voxo/render_test.py
+++ src/voxo/render_test.py
@@ -1,15 +1,20 @@
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Any
+from typing import Any, cast
 
-import glm
 import moderngl
 import moderngl_window
-from glm import mat4 as Mat4  # noqa: N812
+import numpy as np
+from moderngl import Program
 from moderngl_window import geometry
 from moderngl_window.context.base import KeyModifiers
 from moderngl_window.opengl.vao import VAO
+from moderngl_window.scene import Camera
 from moderngl_window.scene.camera import KeyboardCamera
+from pyglm import glm
+from pyglm.glm import mat4x4 as Mat4  # noqa: N812
+
+SIZE = 10
 
 
 class CameraWindow(moderngl_window.WindowConfig):  # type: ignore[misc, name-defined]
@@ -56,7 +61,28 @@ class Object:
 
     @property
     def modelview(self) -> Mat4:
-        return self.translation @ self.rotation @ self.scale
+        return cast("Mat4", self.translation @ self.rotation @ self.scale)
+
+
+class VoxelRenderer:
+    def __init__(self, window: moderngl_window.WindowConfig) -> None:  # type: ignore[name-defined]
+        self.program: Program = window.load_program("programs/raytrace_voxels.glsl")
+        self.geometry: VAO = geometry.quad_fs(normals=False, uvs=True)
+        data = np.random.randint(0, 256, size=(SIZE, SIZE, SIZE), dtype=np.uint8).tobytes()  # noqa: NPY002
+        self.voxel_texture = window.ctx.texture3d((SIZE, SIZE, SIZE), data=data, components=1, alignment=1, dtype="f1")
+        self.voxel_texture.filter = (moderngl.NEAREST, moderngl.NEAREST)
+
+    def render(self, camera: Camera) -> None:
+        ctx = self.voxel_texture.ctx
+        ctx.disable(moderngl.DEPTH_TEST)
+        self.program["uInvProjection"].write(glm.inverse(camera.projection.matrix))  # type:ignore[union-attr]
+        self.program["uInvView"].write(glm.inverse(camera.matrix))  # type:ignore[union-attr]
+        self.program["uCameraPos"].write(camera.position)  # type:ignore[union-attr]
+        self.program["u_voxel_data"].value = 0  # type:ignore[union-attr]
+
+        self.voxel_texture.use(location=0)
+        self.geometry.render(self.program)
+        ctx.enable(moderngl.DEPTH_TEST)
 
 
 class CubeSimple(CameraWindow):
@@ -67,12 +93,13 @@ class CubeSimple(CameraWindow):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
         self.wnd.mouse_exclusivity = True
+        self.full_screen_shader = VoxelRenderer(self)
         self.prog = self.load_program("programs/cube_simple.glsl")
         self.prog["color"].value = 1.0, 1.0, 0.0, 1.0
 
-        self.cube = Object(geometry.cube(size=(2, 2, 2)))
-        self.cube.translation = glm.translate(glm.vec3(0.0, -1.0, 0.0))
-        self.cube.scale = glm.scale(glm.vec3(10.0, 0.1, 10.0))
+        self.cube = Object(geometry.cube(size=(1, 1, 1)))
+        self.cube.translation = glm.translate(glm.vec3(0.0))
+        self.cube.scale = glm.scale(glm.vec3(SIZE))
 
     def on_render(self, time: float, frametime: float) -> None:  # noqa: ARG002
         self.ctx.enable_only(moderngl.CULL_FACE | moderngl.DEPTH_TEST)
@@ -81,4 +108,8 @@ class CubeSimple(CameraWindow):
         self.prog["m_model"].write(self.cube.modelview)
         self.prog["m_camera"].write(self.camera.matrix)
 
+        self.full_screen_shader.render(self.camera)
+
+        self.ctx.wireframe = True
         self.cube.geometry.render(self.prog)
+        self.ctx.wireframe = False
