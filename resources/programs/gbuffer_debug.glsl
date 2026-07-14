970804b87f0839e2fcca3f645578d7ce67837b76
David Westreicher <david.westreicher@gmail.com>
2026-07-15 00:31:58 +0200

    remove other tonemapping

--- resources/programs/gbuffer_debug.glsl
+++ resources/programs/gbuffer_debug.glsl
@@ -47,23 +47,6 @@ vec3 lumaBasedReinhardToneMapping(vec3 color)
     return color;
 }
 
-vec3 Uncharted2ToneMapping(vec3 color) {
-    float A = 0.15;
-    float B = 0.50;
-    float C = 0.10;
-    float D = 0.20;
-    float E = 0.02;
-    float F = 0.30;
-    float W = 11.2;
-    float exposure = 2.;
-    color *= exposure;
-    color = ((color * (A * color + C * B) + D * E) / (color * (A * color + B) + D * F)) - E / F;
-    float white = ((W * (A * W + C * B) + D * E) / (W * (A * W + B) + D * F)) - E / F;
-    color /= white;
-    color = pow(color, vec3(1. / gamma));
-    return color;
-}
-
 void main() {
     vec2 local = fract(uv * 2.0);
     vec3 color;
